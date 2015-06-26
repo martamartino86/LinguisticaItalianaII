9925564f84fe2923e1af723cb331c86560682993
Marta <martamartino86@gmail.com>
2015-06-26 15:20:37 +0200

    First steps
    implemented struct for songs, it could make sense... maybe...

--- search.js
+++ search.js
@@ -1,15 +1,53 @@
-var indexes = [];
-var result;
+var radice;
+var songstruct = [];
 var file = "songs.xml";
 
-// function fillIndexes() {
-// 	Papa.parse(file, {
-// 		complete: function(results) {
-// 			indexes = results;
-// 		}
-// 	});
+// DEBUG
+var debug;
 
-// }
+function containsKey(k) {
+   for (var i = 0; i < songstruct.length; i++) {
+      if (songstruct[i].key === k) {
+         console.log("key trovata")
+         return i;
+      }
+   }
+   console.log("key non trovata")
+   return -1;
+}
+
+// costruttore oggetto Song
+function Song() {
+   this.artist;
+   this.title;
+   this.keys = [];
+   this.text;
+
+   this.inizializza = function(xmlsong) {
+      this.artist = xmlsong.getElementsByTagName("artist")[0].firstChild.nodeValue;
+      this.title  = xmlsong.getElementsByTagName("title")[0].firstChild.nodeValue;
+      this.text   = xmlsong.getElementsByTagName("text")[0].firstChild.nodeValue;
+      var chiavi  = xmlsong.getElementsByTagName("key");
+      for (var i = 0; i < chiavi.length; i++) {
+         this.keys[i] = chiavi[i].firstChild.nodeValue;
+      }
+      // per ogni chiave della canzone, devo controllare se esiste già in songstruct ed inserirla dove serve
+      for (var i = 0; i < this.keys.length; i++) {
+         console.log(this.keys[i]);
+         var pos = (containsKey(this.keys[i]));
+         if (pos == -1) {
+            console.log("aggiungo chiave e primo elemento")
+            var elem = {key: this.keys[i], songs: new Array(this)};
+            songstruct.push(elem);
+         }
+         else {
+            console.log("esiste già")
+            songstruct[pos].songs.push(this);
+         }
+      }
+      
+   }
+}
 
 function caricaXML(nomeFile) {
    var xmlhttp;
@@ -26,9 +64,16 @@ function caricaXML(nomeFile) {
 }
 
 $("document").ready(function(){
-   result = caricaXML(file);
-   console.log(result);
+   var songs = [];
+   radice = caricaXML(file);
+   songs = radice.getElementsByTagName("song");
+   for (var i = 0; i < songs.length; i++) {
+      var s = new Song();
+      s.inizializza(songs[i]);
+   }
+   
+   console.log("uscito dal for")
+
 	$("#searchbtn").click(function(){
-		console.log("CLICK")
 	});
 });
